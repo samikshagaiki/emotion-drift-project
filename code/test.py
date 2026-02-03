bce1d56e2f905d64dbcb97ac88b059790845de5b
samikshagaiki <samikshagaiki@gmail.com>
2026-02-03 22:23:48 +0530

    Initial Project Structure

---
+++ code/test.py
@@ -0,0 +1,8 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+print("All libraries installed successfully!")
